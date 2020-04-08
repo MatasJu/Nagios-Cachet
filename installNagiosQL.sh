c1c7b0bdf9c562e04e5cd11fded4c97235f78db6
MatasJu <33428966+MatasJu@users.noreply.github.com>
2020-04-08 14:31:59 +0200

    Update installNagiosQL.sh

--- installNagiosQL.sh
+++ installNagiosQL.sh
@@ -7,7 +7,7 @@ apt update && apt install -y \
 # nagiosql 
 cd /usr/local/nagios/share
 
-wget -N nagiosql.tar.gz https://gitlab.com/wizonet/nagiosql/-/archive/3.4.1/nagiosql-3.4.1.tar.gz
+wget -O nagiosql.tar.gz https://gitlab.com/wizonet/nagiosql/-/archive/3.4.1/nagiosql-3.4.1.tar.gz
 tar xzvf nagiosql.tar.gz
 mv nagiosql-3.4.1/* webadmin
 rm -rf nagiosql-3.4.1/
