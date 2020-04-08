9e5158d36439a45dd13c3d7e20a9604bc4a33ad0
MatasJu <33428966+MatasJu@users.noreply.github.com>
2020-04-08 13:22:39 +0200

    Update installNagiosQL.sh

--- installNagiosQL.sh
+++ installNagiosQL.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+
+#prereq
 apt update && apt install -y \
-	mariadb-server
+	mariadb-server php-xml
 	
+	
+cd /tmp/
+wget http://pear.php.net/go-pear.phar
+php go-pear.phar
+
+
+
+# nagiosql 
 cd /var/www/html/
 
 wget -N -O nagiosql.tar.gz https://gitlab.com/wizonet/nagiosql/-/archive/3.4.1/nagiosql-3.4.1.tar.gz
 tar xzvf nagiosql.tar.gz
+mv nagiosql-3.4.1/* .
+rmdir nagiosql-3.4.1/
 
 
 # got to //<host>/install/index.php
