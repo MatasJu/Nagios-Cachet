63b68aab36d328db91216fc8d22800dc44c86f79
MatasJu <33428966+MatasJu@users.noreply.github.com>
2020-04-08 11:51:24 +0200

    Update InstallNagios.sh

--- InstallNagios.sh
+++ InstallNagios.sh
@@ -3,15 +3,15 @@
 
 sudo apt update && apt install -y \
 	git autoconf gcc libc6 make wget unzip apache2 apache2-utils php libgd-dev  \
-    libmcrypt-dev libssl-dev bc gawk dc build-essential snmp libnet-snmp-perl gettext \ 
-	iptables-persistent && \
+    libmcrypt-dev libssl-dev bc gawk dc build-essential snmp libnet-snmp-perl gettext iptables-persistent && \
 	apt clean && rm -Rf /var/lib/apt/lists/*
 	
 cd /tmp
 wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.5.tar.gz
-tar xzf nagioscore.tar.gz -C /tmp/nagioscore
 
-cd /tmp/nagioscore
+tar xzf nagioscore.tar.gz nagioscore -C /tmp/nagioscore
+
+cd /tmp/nagioscore-nagios-4.4.5
 ./configure --with-httpd-conf=/etc/apache2/sites-enabled
 make all
 
@@ -33,9 +33,9 @@ htpasswd -bc /usr/local/nagios/etc/htpasswd.users nagiosadmin nagiosadmin
 
 cd /tmp
 wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
-tar zxf nagios-plugins.tar.gz -C /tmp/nagios-plugins
+tar zxf nagios-plugins.tar.gz 
 
-cd /tmp/nagios-plugins
+cd /tmp/nagios-plugins-release-2.2.1
 ./tools/setup
 ./configure
 make
