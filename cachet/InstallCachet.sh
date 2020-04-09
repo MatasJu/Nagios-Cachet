91128ee5e2a669e7e26d7247921b2f8c25f22599
MatasJu <33428966+MatasJu@users.noreply.github.com>
2020-04-09 16:59:26 +0200

    update

--- cachet/InstallCachet.sh
+++ cachet/InstallCachet.sh
@@ -1,11 +1,36 @@
+@@ -1,27 +0,0 @@
 #!/bin/bash
+
 SCRIPT_LOCATION="$(pwd)"
 
-mkdir tmp &&  cd tmp
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+apt update && install php-mbstring php-apcu php-apcu-bc
 
 
-sudo apt install -y libffi-dev libssl-dev
-sudo apt install -y python3 python3-pip
-sudo pip3 install docker-compose
+mysql -u root -p -e "create database cachet"
+
+cd /var/www
+git clone https://github.com/cachethq/Cachet.git
+cd Cachet
+
+git checkout v2.4
+
+cp $SCRIPT_LOCATION/.env .
+
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+composer install --no-dev -o
+
+echo "dont configure!"
+php artisan cachet:install
+php artisan key:generate
+
+chmod -R 777 storage
+rm -rf bootstrap/cache/*
+chmod -R 777 bootstrap/
+
+a2dissite 000-default
+systemctl reload apache2
+
+cp $SCRIPT_LOCATION/cachet.conf /etc/apache2/sites-available/
+a2ensite cachet
+systemctl restart apache2
