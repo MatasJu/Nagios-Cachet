04af237ad3b4b8b929675c437215b771cc3de3f0
MatasJu <s-mjurev@haw-landshut.de>
2020-04-09 13:37:57 +0200

    update

--- InstallCachet.sh
+++ InstallCachet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SCRIPT_LOCATION = "$(PWD)"
+SCRIPT_LOCATION="$(pwd)"
+
 cd /var/www
 git clone https://github.com/cachethq/Cachet.git
 cd Cachet
@@ -11,8 +12,4 @@ cp $SCRIPT_LOCATION/.env .
 
 
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-
-
-
-
-
+composer install --no-dev -o
