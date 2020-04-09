3158d037a70fa6284066fd05cabf2773e3ac3052
MatasJu <33428966+MatasJu@users.noreply.github.com>
2020-04-09 14:37:54 +0200

    Update InstallCachet.sh

--- cachet/InstallCachet.sh
+++ cachet/InstallCachet.sh
@@ -4,3 +4,8 @@ SCRIPT_LOCATION = "$(pwd)"
 mkdir tmp &&  cd tmp
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+
+sudo apt install -y libffi-dev libssl-dev
+sudo apt install -y python3 python3-pip
+sudo pip3 install docker-compose
