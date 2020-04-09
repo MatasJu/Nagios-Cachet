cb6f336df0b8f1d58ae444fd37da148f5c735b30
MatasJu <33428966+MatasJu@users.noreply.github.com>
2020-04-09 19:31:20 +0200

    Update InstallNagios.sh

--- InstallNagios.sh
+++ InstallNagios.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-
+SCRIPT_LOCATION="$(pwd)"
 #set timezone for host
 timedatectl set-timezone Europe/Berlin
 
 # Nagios Core and Nagios Plugins prerequisites 
 sudo apt update && apt install -y \
 	git autoconf gcc libc6 make wget unzip apache2 apache2-utils php libgd-dev  \
-    libmcrypt-dev libssl-dev bc gawk dc build-essential snmp libnet-snmp-perl gettext iptables-persistent && \
+    libmcrypt-dev libssl-dev bc gawk dc build-essential snmp libnet-snmp-perl gettext && \
 	apt clean && rm -Rf /var/lib/apt/lists/*
 	
-cd /tmp
-wget -N nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.5.tar.gz
 
-tar xzvf nagioscore.tar.gz -C /tmp/nagioscore
+wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.5.tar.gz
 
-cd /tmp/nagioscore-nagios-4.4.5
+tar xzvf nagioscore.tar.gz
+
+cd nagioscore-nagios-4.4.5/
 ./configure --with-httpd-conf=/etc/apache2/sites-enabled
 make all
 
@@ -34,11 +34,11 @@ htpasswd -bc /usr/local/nagios/etc/htpasswd.users nagiosadmin nagiosadmin
 
 # Nagios Plugins
 
-cd /tmp
+cd $SCRIPT_LOCATION
 wget -N --no-check-certificate nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
 tar zxvf nagios-plugins.tar.gz 
 
-cd /tmp/nagios-plugins-release-2.2.1
+cd /nagios-plugins-release-2.2.1
 ./tools/setup
 ./configure
 make
@@ -46,7 +46,7 @@ make install
 
 
 
-echo 'add [date.timezone = "Europe/Berlin] to nano etc/php/7.3/apache2/php.ini' 
+echo 'date.timezone = "Europe/Berlin"' >> /etc/php/7.3/apache2/php.ini 
 
 systemctl enable apache2.service && systemctl restart apache2.service
 
